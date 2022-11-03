2f1593942134818058fe4f66ed04c09f9375fcd6
BigBoi12348 <107095940+BigBoi12348@users.noreply.github.com>
2022-11-03 15:12:51 +1100

    BigExplosions

---
+++ Networking/Assets/Scripts/Break.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break : MonoBehaviour
+{
+    public GameObject Block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            BreahTheThing();
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Bullet(Clone)"))
+        {
+            BreahTheThing();
+            Debug.Log("Hit");
+        }
+        if (other.tag.Equals("Blow"))
+        {
+            BreahTheThing();
+            Debug.Log("BANG");
+        }
+    }
+    public void BreahTheThing()
+    {
+        Instantiate(Block, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
